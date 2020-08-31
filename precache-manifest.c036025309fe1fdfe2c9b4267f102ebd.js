42596b6ee3bb9399af50e741e82a1c1e8be3146f
amuyassar17 <amuyassar16@gmail.com>
2020-08-31 20:10:57 +0800

    Updates

---
+++ precache-manifest.c036025309fe1fdfe2c9b4267f102ebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a42dca8e16f38da34ee2225fefe0e9",
+    "url": "/ahmad-card-challenge/index.html"
+  },
+  {
+    "revision": "1e1db51e60e692c68ab2",
+    "url": "/ahmad-card-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8af425e760ba9b53b254",
+    "url": "/ahmad-card-challenge/static/js/2.1c408fee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ahmad-card-challenge/static/js/2.1c408fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1db51e60e692c68ab2",
+    "url": "/ahmad-card-challenge/static/js/main.e76660b1.chunk.js"
+  },
+  {
+    "revision": "a462f83062853745f32c",
+    "url": "/ahmad-card-challenge/static/js/runtime-main.d62a0023.js"
+  },
+  {
+    "revision": "07a56f849578a22efef8256193c47820",
+    "url": "/ahmad-card-challenge/static/media/acca.07a56f84.jpg"
+  }
+]);
